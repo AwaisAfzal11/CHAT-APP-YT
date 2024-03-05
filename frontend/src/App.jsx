43c815b0001941eb4125318d59cb884bcd1b713e
Awais Afzal <92658196+AwaisAfzal11@users.noreply.github.com>
2024-03-05 17:11:42 +0500

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+
+function App() {
+  return (
+    <>
+      <button className="btn btn-outline">Default</button>
+      <button className="btn btn-outline btn-primary">Primary</button>
+      <button className="btn btn-outline btn-secondary">Secondary</button>
+      <button className="btn btn-outline btn-accent">Accent</button>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
