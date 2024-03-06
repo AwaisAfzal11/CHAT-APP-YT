280371df50c42bf64989c320badb8c77859800a3
Awais Afzal <92658196+AwaisAfzal11@users.noreply.github.com>
2024-03-06 15:29:57 +0500

    Message system

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Sidebar from '../../components/sidebar/Sidebar'
+import Messages from '../../components/messages/Messages'
 
 const Home = () => {
   return (
     <div className='flex sm:h-[450px] md:h-[550px] rounded-1g overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-1g bg-opacity-0'>
       <Sidebar/>
+      <Messages/>
     </div>
   )
   
